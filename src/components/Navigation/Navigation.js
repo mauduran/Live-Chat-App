bada9b60a563adb5ecc9c734b8d57769cb5c2792
mauduran <duran@MBP-Duran.local>
2020-06-25 22:29:19 -0500

    Added some code for navbar

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navigation() {
+    return (
+        <nav id="navbar">
+            <div>
+                
+            </div>
+        </nav>
+    )
+}
