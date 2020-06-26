6d15593d82ef0180f517933c10ee818a1e1ad08b
mauduran <duran@MBP-Duran.local>
2020-06-26 15:12:48 -0500

    Modified HomeMessage container and css file to folllow good practices

---
+++ src/components/Home/HomeMessage/HomeMessage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './HomeMessage.css'
+const HomeMessage = () => {
+    return (
+        <div style={{height: '40%', width: '35%'}}>
+            <div id="homeMessage">
+                <h1>Small Talk</h1>
+                <h2>Ready to talk?</h2>
+                <h3>The world is one message away</h3>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default HomeMessage;
