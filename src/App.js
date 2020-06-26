ec21af31c38ddc3865c7b5df18fa0e0b8b2b1e59
JPRR44 <juanp.robles@hotmail.com>
2020-06-25 22:08:01 -0500

    Register component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import register from './components/Register/register'
 import './styles/App.css';
-
-
-/// Comment 2
 
 function App() {
   return (
     <div className="App">
       <Router >
         <Switch>
-          <Route exact path="/" render={() => { return <h1> Home</h1> }} />
+          {/* <Route exact path="/" render={() => { return <h1> Home</h1> }} /> */}
+          <Route exact path="/" render={register} />
         </Switch>
       </Router>
     </div>
