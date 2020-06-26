a3265a37c138711999fc27f1d5cadbb81fb4c1ff
mauduran <duran@MBP-Duran.local>
2020-06-26 15:14:18 -0500

    Created Home container

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import BackgroundImage from '../../assets/img/backgroundHome.jpeg';
+import './Home.css'
+import Register from '../../components/Home/Register/Register';
+
+import { Container, Row, Col } from 'reactstrap';
+import HomeMessage from '../../components/Home/HomeMessage/HomeMessage';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div id="home" style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover' }}>
+                <Container style={{display: "flex", height: '97%', maxWidth: '100%', margin: "0", justifyContent: "space-around", alignItems: "center"}}>
+                            <HomeMessage id='colRegisterMessage' />
+
+                            <Register />
+                </Container>
+
+                <div style={{color: "#aaa",  height:"3%", width: "100%", backgroundColor: "#383838", textAlign: "center"}}>
+                    Created by Mauricio Durán & Juan Pablo Ramos
+                </div>
+            </div>
+        )
+    }
+}
