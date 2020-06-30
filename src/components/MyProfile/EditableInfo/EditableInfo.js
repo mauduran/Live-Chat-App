7f77f7cff577bbadf614b7b26343793f09011d91
JPRR44 <juanp.robles@hotmail.com>
2020-06-30 12:35:38 -0500

    Changes on MyProfile containner

--- src/components/MyProfile/EditableInfo/EditableInfo.js
+++ src/components/MyProfile/EditableInfo/EditableInfo.js
@@ -4,15 +4,15 @@ import { Form, FormGroup, Label, Input } from 'reactstrap';
 const EditableInfo = () => {
     return(
         <Form>
-        <FormGroup>
-            <Label for="locationProfile">Location</Label>
-            <Input type="text" name="locationProfile" id="locationProfile" placeholder="Enter your location" />
-        </FormGroup>
-        <FormGroup>
-            <Label for="quoteProfile">Quote</Label>
-            <Input type="text" name="quoteProfile" id="quoteProfile" placeholder="Enter your quote" />
-        </FormGroup>      
-    </Form>
+            <FormGroup>
+                <Label for="locationProfile">Location</Label>
+                <Input type="text" name="locationProfile" id="locationProfile" placeholder="Enter your location" />
+            </FormGroup>
+            <FormGroup>
+                <Label for="quoteProfile">Quote</Label>
+                <Input type="text" name="quoteProfile" id="quoteProfile" placeholder="Enter your quote" />
+            </FormGroup>      
+        </Form>
     )
 }
 
--- src/containers/MyProfile/MyProfile.js
+++ src/containers/MyProfile/MyProfile.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import BackgroundImage from '../../assets/img/backgroundHome.jpeg';
-import { Container } from 'reactstrap';
+import PP from '../../components/MyProfile/PP/PP'
+import ProfileInfo from '../../components/MyProfile/ProfileInfo/ProfileInfo'
 import EditableInfo from '../../components/MyProfile/EditableInfo/EditableInfo'
+import { Form, FormGroup,Container, Row, Col } from 'reactstrap';
+import './MyProfile.css'
+import LoginMessage from '../../components/LoginMessage/LoginMessage';
+
+
 
 export default class Home extends Component {
-    render() {
+    render() { 
         return (
-            <div id="Login" style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover'}}>
-                <Container style={{display: "flex", height: '97%', maxWidth: '100%', margin: "0", justifyContent: "space-around", alignItems: "center"}}>
-                    <EditableInfo/>
-                </Container>
+            <div id="MyPrifile" style={{ backgroundImage: `url(${BackgroundImage})`, backgroundSize: 'cover'}}>
+                <Container id = "containerMyProfile">
+                        <Form id = "formProfile">
+                            <Row>
+                                <Col>
+                                    <FormGroup>  
+                                        <PP/>
+                                    </FormGroup>
+                                </Col>
+                                <Col>
+                                    <FormGroup>
+                                        <ProfileInfo/>
+                                    </FormGroup> 
+                                </Col>
+                            </Row> 
+                            <Row>
+                                <Col><EditableInfo/></Col>
+                            </Row>
+                        </Form>
+                    </Container>
+                    <LoginMessage/>
             </div>
         )
-    }
+    } 
 }
