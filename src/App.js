416ecb46d992354724d30ec6593caf8d677907f2
mauduran <duran@MBP-Duran.local>
2020-06-29 23:33:31 -0500

    Added created by banner to app for all components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './styles/App.css';
 import Navigation from './components/Navigation/Navigation';
 import Home from './containers/Home/Home';
+import UserSearch from './containers/UserSearch/UserSearch';
 
 function App() {
   const [isLogged, setisLogged] = useState(false);
@@ -13,10 +14,14 @@ function App() {
         <Navigation isLogged={isLogged} setisLogged={setisLogged} />
         <div id="content">
           <Switch>
-            <Route exact path="/" render={()=><Home/>} />
+            <Route exact path="/" render={() => <Home />} />
+            <Route path="/search/:searchQuery" render={() => <UserSearch />} />
           </Switch>
         </div>
       </Router>
+      <div style={{ color: "#aaa", height: "3%", width: "100%", backgroundColor: "#383838", textAlign: "center" }}>
+        Created by Mauricio Durán & Juan Pablo Ramos
+      </div>
     </div>
   );
 }
