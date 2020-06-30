ac0fa0e54a3ce00b80c00e19f46f330518a76e63
JPRR44 <juanp.robles@hotmail.com>
2020-06-30 15:43:45 -0500

    Added buttons to MyProfile

--- src/components/MyProfile/EditableInfo/EditableInfo.js
+++ src/components/MyProfile/EditableInfo/EditableInfo.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Container, Row, Col, Button  } from 'reactstrap';
 import './EditableInfo.css';
 
 const EditableInfo = () => {
     return(
-        <Form id = "formEditable">
-            <FormGroup>
-                <Label for="locationProfile">Location</Label>
-                <Input type="text" name="locationProfile" id="locationProfile" placeholder="Enter your location" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="quoteProfile">Quote</Label>
-                <Input type="text" name="quoteProfile" id="quoteProfile" placeholder="Enter your quote" />
-            </FormGroup>      
-        </Form>
+        <Container>
+            <Row>
+                <Form id = "formEditable">
+                    <FormGroup>
+                        <Label for="locationProfile">Location</Label>
+                        <Input type="text" name="locationProfile" id="locationProfile" placeholder="Enter your location" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="quoteProfile">Quote</Label>
+                        <Input type="text" name="quoteProfile" id="quoteProfile" placeholder="Enter your quote" />
+                    </FormGroup>      
+                </Form>
+            </Row>
+            <Row id = "rowButtons">
+                <Col><Button color="success" style={{width: '100%'}}> Update</Button></Col>
+                <Col><Button color="danger" style={{width: '100%'}}>Cancel</Button></Col>
+            </Row>
+        </Container>
     )
 }
 
