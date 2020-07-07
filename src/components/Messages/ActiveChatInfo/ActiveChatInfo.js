354a3e422c26b8dfa35934ac8135eadbdf5c3d58
mauduran <duran@MBP-Duran.local>
2020-07-06 23:01:09 -0500

    Created active chat info component

---
+++ src/components/Messages/ActiveChatInfo/ActiveChatInfo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ActiveChatInfo() {
+    return (
+        <div style={{ width: "25%", height: "100%", backgroundColor: '#f8f8f8'}}>
+            
+        </div>
+    )
+}
