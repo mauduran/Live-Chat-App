661445245b9f5a809e6ae281b264597f0820408f
mauduran <duran@MBP-Duran.local>
2020-07-06 22:59:07 -0500

    Created active chat messages, component where all messages in a conversation are shown

---
+++ src/components/Messages/ActiveChatMessages/ActiveChatMessages.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+import Message from '../Message/Message';
+
+export default function ActiveChatMessages({ messages, activeConversation }) {
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+
+    useEffect(scrollToBottom, [messages]);
+
+    return (
+        <div style={{ width: "100%", height: 'calc(100% - 50px)', overflow: "scroll", padding: "20px", boxSizing: 'border-box' }}>
+            {
+                messages.filter(el=>el.roomId===activeConversation.conversationId).map((message, id) => {
+                    return (
+                        <Message message={message} key={id} />
+                    )
+                })
+            }
+            <div ref={messagesEndRef}>
+            </div>
+        </div>
+    )
+}
