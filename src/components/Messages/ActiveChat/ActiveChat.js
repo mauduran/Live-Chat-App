e4daa6b2a41adc566e95e6476d0f4a9862a85ad3
mauduran <duran@MBP-Duran.local>
2020-07-14 23:00:04 -0500

    Added message type to each message

--- src/components/Messages/ActiveChat/ActiveChat.js
+++ src/components/Messages/ActiveChat/ActiveChat.js
@@ -7,42 +7,49 @@ import ActiveChatInput from '../ActiveChatInput/ActiveChatInput';
 const dummyMessages = [
     {
         roomId: 1,
+        type: 'message',
         body: "Hola, cómo estás?",
         date: new Date(2019, 6, 5, 12, 24, 0),
         sender: "mau4duran"
     },
     {
         roomId: 1,
+        type: 'message',
         body: "Qué tal, buen hombre.",
         date: new Date(2020, 6, 4, 14, 14, 30),
         sender: "jprr44"
     },
     {
         roomId: 1,
+        type: 'message',
         body: "Cómo vamos con el sitio?",
         date: new Date(2020, 6, 5, 14, 34, 0),
         sender: "mau4duran"
     },
     {
         roomId: 1,
+        type: 'message',
         body: "Bien",
         date: new Date(2020, 6, 5, 14, 35, 0),
         sender: "jprr44"
     },
     {
         roomId: 1,
+        type: 'message',
         body: "Falta lo de mensajes.",
         date: new Date(2020, 6, 5, 12, 35, 20),
         sender: "jprr44"
     },
     {
         roomId: 1,
+        type: 'message',
         body: "Voy",
         date: new Date(2020, 6, 5, 12, 44, 0),
         sender: "mau4duran"
     },
     {
         roomId: 2,
+        type: 'message',
         body: "Voy",
         date: new Date(2020, 6, 5, 12, 44, 0),
         sender: "mau4duran"
@@ -50,6 +57,7 @@ const dummyMessages = [
 
     {
         roomId: 3,
+        type: 'message',
         body: "qué pedo",
         date: new Date(2020, 6, 5, 10, 40, 0),
         sender: "jsfran"
@@ -57,9 +65,17 @@ const dummyMessages = [
 
     {
         roomId: 4,
+        type: 'message',
         body: "heyyy",
         date: new Date(2020, 7, 5, 19, 40, 0),
         sender: "jsfran"
+    },
+    {
+        roomId: 4,
+        type: 'leave-conversation',
+        body: "",
+        date: new Date(),
+        sender: "mau4duran"
     }
 ];
 
@@ -75,7 +91,7 @@ export default function ActiveChat({ activeConversation }) {
                     <ActiveChatMessages activeConversation={activeConversation} messages={messages} />
                     <ActiveChatInput/>
                 </div>
-                <ActiveChatInfo />
+                <ActiveChatInfo activeConversation={activeConversation}  />
             </div>
         </div>
     )
