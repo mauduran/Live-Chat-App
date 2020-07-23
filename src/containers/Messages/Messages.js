af3f7b8dddd2bb3a5fe507d61ac4931e54edaf39
mauduran <duran@MBP-Duran.local>
2020-07-22 22:45:45 -0500

    Added socket pprop

--- src/containers/Messages/Messages.js
+++ src/containers/Messages/Messages.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './Messages.css';
 import Conversations from '../../components/Messages/Conversations/Conversations';
 
-export default function Messages({ user, setuser }) {
+export default function Messages({ user, setuser, socket }) {
     return (
         <div className="messagesContainer">
-            <Conversations user={user}/>
+            <Conversations user={user} socket={socket}/>
         </div>
     )
 }
