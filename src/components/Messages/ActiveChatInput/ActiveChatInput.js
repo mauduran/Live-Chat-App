101cc32741c91d12c5cdd8d08bbb4da4b0d5c7b9
mauduran <duran@MBP-Duran.local>
2020-07-22 22:44:28 -0500

    send message functionality

--- src/components/Messages/ActiveChatInput/ActiveChatInput.js
+++ src/components/Messages/ActiveChatInput/ActiveChatInput.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Input, Button } from 'reactstrap';
 
-export default function ActiveChatInput() {
+export default function ActiveChatInput({ activeConversation, socket }) {
+    const [messageInput, setmessageInput] = useState('');
+
+    const handleSendMessage = (e) => {
+        if (e.key === 'Enter') {
+            sendMessage();
+        }
+    }
+
+    const sendMessage = () => {
+        if (messageInput) {
+
+            const dataObj = {
+                roomId: activeConversation.conversationId,
+                type: 'message',
+                body: messageInput,
+            }
+            socket.emit('message', dataObj);
+        }
+    }
+
     return (
         <div style={{ width: '90%', margin: '0 auto', height: '50px' }}>
-            <Input style={{ width: "100%", paddingRight: '50px', display: 'inline-block' }} type="text" name="messageInput" id="messageInput" placeholder="" />
-            <Button style={{ width: "50px", marginLeft: '-50px' }}><i className="fas fa-paper-plane"></i></Button>
+            <Input
+                style={{ width: "100%", paddingRight: '50px', display: 'inline-block' }}
+                type="text"
+                name="messageInput"
+                id="messageInput"
+                placeholder=""
+                value={messageInput}
+                onChange={(e) => setmessageInput(e.target.value)}
+                onKeyUp={handleSendMessage}
+            />
+
+            <Button onClick={sendMessage} style={{ width: "50px", marginLeft: '-50px' }}><i className="fas fa-paper-plane"></i></Button>
         </div>
     )
 }
