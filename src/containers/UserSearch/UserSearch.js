f5c545d3093fe2f2c96f810b5a681c26459a389f
mauduran <duran@MBP-Duran.local>
2020-07-28 21:25:57 -0500

    Removed Dummy users and made API call to backend

--- src/containers/UserSearch/UserSearch.js
+++ src/containers/UserSearch/UserSearch.js
@@ -1,78 +1,87 @@
-import React, { uuseEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import UserResultCard from '../../components/UserSearch/UserResultCard';
 import './UserSearch.css'
 import BackButton from '../../components/BackButton/BackButton'
 
-const users = [
-    {
-        userId: 1,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "John Doe",
-        username: "iamjohn",
-        location: "Guadalajara, Jal",
-        joined: new Date('10/21/2015'),
-        status: "Meaningful Quote goes here"
-    },
-    {
-        userId: 2,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Mauricio Duran",
-        username: "mau4duran",
-        location: "San Luis Potosí, SLP",
-        joined: new Date('06/14/2018'),
-        status: "Meaningful Quote goes here"
-    },
-    {
-        userId: 3,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Juan Pablo Ramos",
-        username: "jprr44",
-        location: "Guadalajara, Jal",
-        joined: new Date('08/31/2020'),
-        status: "I like big dicks"
-    },
-    {
-        userId: 4,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "José Francisco González",
-        username: "jsfran",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-    {
-        userId: 5,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Marcelo Londra",
-        username: "marselop",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-    {
-        userId: 6,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Marco Galina",
-        username: "marko",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-]
+// const users = [
+//     {
+//         userId: 1,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "John Doe",
+//         username: "iamjohn",
+//         location: "Guadalajara, Jal",
+//         joined: new Date('10/21/2015'),
+//         status: "Meaningful Quote goes here"
+//     },
+//     {
+//         userId: 2,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "Mauricio Duran",
+//         username: "mau4duran",
+//         location: "San Luis Potosí, SLP",
+//         joined: new Date('06/14/2018'),
+//         status: "Meaningful Quote goes here"
+//     },
+//     {
+//         userId: 3,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "Juan Pablo Ramos",
+//         username: "jprr44",
+//         location: "Guadalajara, Jal",
+//         joined: new Date('08/31/2020'),
+//         status: "I like big dicks"
+//     },
+//     {
+//         userId: 4,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "José Francisco González",
+//         username: "jsfran",
+//         location: "Guadalajara, Jal",
+//         joined: new Date('09/14/2016'),
+//         status: "Lorem ipsun dolor immet"
+//     },
+//     {
+//         userId: 5,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "Marcelo Londra",
+//         username: "marselop",
+//         location: "Guadalajara, Jal",
+//         joined: new Date('09/14/2016'),
+//         status: "Lorem ipsun dolor immet"
+//     },
+//     {
+//         userId: 6,
+//         imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
+//         name: "Marco Galina",
+//         username: "marko",
+//         location: "Guadalajara, Jal",
+//         joined: new Date('09/14/2016'),
+//         status: "Lorem ipsun dolor immet"
+//     },
+// ]
 
 
 function UserSearch({ match }) {
+    const [userResults, setuserResults] = useState([])
+    useEffect(() => {
+        if (match.params.searchQuery) {
+            fetch(`http://localhost:3001/users?searchInput=${match.params.searchQuery.toLowerCase()}`)
+                .then(res => res.json())
+                .then(usrs => setuserResults(usrs))
+                .catch(console.log)
+        }
+
+    }, [match.params.searchQuery])
 
     const input = (match.params.searchQuery) ? match.params.searchQuery.toLowerCase() : "";
     return (
         <div className="userResultsContainer">
             <BackButton />
             {
-                users
-                    .filter(user => user.name.toLowerCase().includes(input)
-                        || user.username.toLowerCase().includes(input))
-                    .map(user => <UserResultCard key={user.userId} user={user} />)
+                (userResults) ?
+                    userResults.map(user => <UserResultCard key={user.userId} user={user} />)
+                    : <h1 style={{ textAlign: "center", marginTop: "5%" }}>No users found.</h1>
             }
         </div>
     )
