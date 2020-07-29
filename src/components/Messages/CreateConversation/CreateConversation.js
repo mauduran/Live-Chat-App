287cfc589a24996f4930dfdccf6e15584fae3674
mauduran <duran@MBP-Duran.local>
2020-07-28 22:11:36 -0500

    Removed dummies

--- src/components/Messages/CreateConversation/CreateConversation.js
+++ src/components/Messages/CreateConversation/CreateConversation.js
@@ -1,80 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CreateConvoProfile from '../CreateConvoProfile/CreateConvoProfile';
 import { withRouter } from 'react-router-dom';
 import './CreateConversation.css';
 
-const users = [
-    {
-        userId: 1,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "John Doe",
-        username: "iamjohn",
-        location: "Guadalajara, Jal",
-        joined: new Date('10/21/2015'),
-        status: "Meaningful Quote goes here" 
-    },
-    {
-        userId: 2,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Mauricio Duran",
-        username: "mau4duran",
-        location: "San Luis Potosí, SLP",
-        joined: new Date('06/14/2018'),
-        status: "Meaningful Quote goes here"
-    },
-    { 
-        userId: 3,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Juan Pablo Ramos",
-        username: "jprr44",
-        location: "Guadalajara, Jal",
-        joined: new Date('08/31/2020'),
-        status: "I like big dicks"
-    },
-    {
-        userId: 4,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "José Francisco González",
-        username: "jsfran",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-    {
-        userId: 5,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Marcelo Londra", 
-        username: "marselop",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-    {
-        userId: 6,
-        imgUrl: "https://docs.atlassian.com/aui/5.2-m6/docs/img/user-avatar-blue-96@2x.png",
-        name: "Marco Galina",
-        username: "marko",
-        location: "Guadalajara, Jal",
-        joined: new Date('09/14/2016'),
-        status: "Lorem ipsun dolor immet"
-    },
-]
 
-function CreateConversation({inputText, members, setmembers}){
+function CreateConversation({ inputText, members, setmembers }) {
+    const [users, setusers] = useState();
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/users?searchInput=${inputText}`)
+            .then(res => res.json())
+            .then(usrs => setusers(usrs))
+            .catch(console.log)
+
+    }, [inputText]);
+
     return (
         <div id="CreateConversation">
             {
-                users
-                    .filter(user => (!members.map(member => member.name.toLowerCase()).includes(user.name.toLowerCase()) ||
-                                     !members.map(member => member.username.toLowerCase()).includes(user.username.toLowerCase()))
-                                     && 
-                                     (user.name.toLowerCase().includes(inputText.toLowerCase()) || user.username.toLowerCase().includes(inputText.toLowerCase()))
-                    )
-                    .map(user => <CreateConvoProfile setmembers={setmembers} key={user.userId} user={user} members={members}/>)
+
+                (users) ?
+                    users.filter(user => !members.map(member => member.toLowerCase()).includes(user.username.toLowerCase()))
+                        .map(user => <CreateConvoProfile setmembers={setmembers} key={user.userId} user={user} members={members} />) :
+                    null
             }
         </div>
-            )  
-    }
+    )
+}
 
 export default withRouter(CreateConversation)
 
